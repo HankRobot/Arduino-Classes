4e96179ac6c3fbea0fa57ffe114a32f4ef7a3652
USMC Robotics Club <38729883+usmcroboticsclub@users.noreply.github.com>
2018-07-09 10:57:54 +0800

    Added heart shape POV
    Note that if you want to use the libraries it only supports up to pin 8

--- First Arduino Workshop/POV.ino
+++ First Arduino Workshop/POV.ino
@@ -35,54 +35,84 @@ int X[] = {1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 
 int Y[] = {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0};
 int Z[] = {1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1};
 int* alpha[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N}; //,T,U,V,W,X,Y,Z};
-int letterSpace = 4000;   // defining the space between the letters (ms)
-int dotTime = 3000;       // defining how long the led is lit (ms)
+int a[] = {0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0};
+int letterSpace = 20000;   // defining the space between the letters (ms)
+int dotTime = 2500;// defining how long the led is lit (ms)
 void setup(){
   // setting the ports of the leds to OUTPUT
-  for ( int i = 2; i < 10 ; i++ )
-  { pinMode(i, OUTPUT);     // 8 Led pins in total
+  for ( int i = 2; i < 14 ; i++ )
+  { pinMode(i, OUTPUT);     // 13 Led pins in total
   }
 }
 void printLetter(int letter[])
 {
   int y;
   // printing the first y row of the letter
-  for (y = 0; y < 8; y++)
+  for (y = 0; y < 12; y++)
   {
     digitalWrite(y + 2, letter[y]);               // pinmode begins at pin 2 to pin 9 . letter[y] begins to iterate through the high and lows of the letter assigned in the first place  
   }                                               // for the first 8 PWMS, example: when y=0, pin is 2, letter[0] if is NUMBER8[0] would give 0 value, which is Low, led in pin 2 will not light up.
   delayMicroseconds(dotTime);
   // printing the second y row of the letter
-  for (y = 0; y < 8; y++)
+  for (y = 0; y < 12; y++)
   {
-    digitalWrite(y + 2, letter[y + 8]);            // begins reading at the 9th position of the letter to 17th
+    digitalWrite(y + 2, letter[y + 12]);            // begins reading at the 9th position of the letter to 17th
   }
   delayMicroseconds(dotTime);
   // printing the third y row of the letter         // begins reading at the 17th to 26th
-  for (y = 0; y < 8; y++)
+  for (y = 0; y < 12; y++)
   {
-    digitalWrite(y + 2, letter[y + 16]);
-  }
-  delayMicroseconds(dotTime);
-  for (y = 0; y < 8; y++) {
     digitalWrite(y + 2, letter[y + 24]);
   }
   delayMicroseconds(dotTime);
-  for (y = 0; y < 8; y++) {
-    digitalWrite(y + 2, letter[y + 32]);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 36]);
+  }
+  delayMicroseconds(dotTime*2);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 48]);
+  }
+  delayMicroseconds(dotTime);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 60]);
+  }
+  delayMicroseconds(dotTime*1.3);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 72]);
+  }
+  delayMicroseconds(dotTime);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 84]);
+  }
+  delayMicroseconds(dotTime*2);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 96]);
+  }
+  delayMicroseconds(dotTime);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 108]);
+  }
+  delayMicroseconds(dotTime);
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 120]);
+  }
+  delayMicroseconds(dotTime);
+  /*
+  for (y = 0; y < 12; y++) {
+    digitalWrite(y + 2, letter[y + 132]);
   }
   delayMicroseconds(dotTime);
   // printing the sspace between the letters
-  for (y = 0; y < 8; y++)
+*/
+  for (y = 0; y < 12; y++)
   {
     digitalWrite(y + 2, 0);
   }
   delayMicroseconds(letterSpace);
+
 }
 
 void loop()
 {
-  printLetter (L);
-  printLetter (O);
-  printLetter (L);  
+  printLetter (a);
 }
